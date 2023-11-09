20575eaaf25a639091caf9bf60011417cfc960dd
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-09 20:35:43 +0300

    Part3.Tests.mostFreq

--- src/Part3/Tasks.hs
+++ src/Part3/Tasks.hs
@@ -1,6 +1,8 @@
 module Part3.Tasks where
 
 import Util (notImplementedYet)
+import Data.Ord (comparing)
+import Data.List (maximumBy)
 
 -- Функция finc принимает на вход функцию f и число n и возвращает список чисел [f(n), f(n + 1), ...]
 finc :: (Int -> a) -> Int -> [a]
@@ -12,7 +14,13 @@ ff f x = x : ff f (f x)
 
 -- Дан список чисел. Вернуть самую часто встречающуюся *цифру* в этих числах (если таковых несколько -- вернуть любую)
 mostFreq :: [Int] -> Int
-mostFreq = notImplementedYet
+mostFreq nums =
+    let allDigits = concatMap show nums
+        digits = "0123456789"
+        countDigit d = (d, length (filter (d==) allDigits))
+        timesEach = map countDigit digits
+        mostFreqDigit = fst $ maximumBy (comparing snd) timesEach
+    in read [mostFreqDigit]
 
 -- Дан список lst. Вернуть список элементов из lst без повторений, порядок может быть произвольным.
 uniq :: (Eq a) => [a] -> [a]
