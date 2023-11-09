f1d94f9f3b69df50f2c6d86c6f6633d0a77de34e
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-09 22:18:50 +0300

    Part4.Tests.monad

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -2,6 +2,7 @@
 module Part4.Tasks where
 
 import Util(notImplementedYet)
+import Control.Applicative
 
 -- Перевёрнутый связный список -- хранит ссылку не на последующию, а на предыдущую ячейку
 data ReverseList a = REmpty | (ReverseList a) :< a
@@ -34,9 +35,25 @@ instance Eq a => Eq (ReverseList a) where
     (==) REmpty REmpty = True
     (==) (tailA :< headA) (tailB :< headB) = headA == headB && tailA == tailB
     (==) _ _ = False
-    (/=) a b = not (a == b) 
+    (/=) a b = not (a == b)
+
 instance Semigroup (ReverseList a) where
+  (<>) a REmpty = a
+  (<>) a (tailB :< headB) = a <> tailB :< headB
+
 instance Monoid (ReverseList a) where
+  mempty = REmpty
+
 instance Functor ReverseList where
+  fmap f REmpty = REmpty
+  fmap f (tail :< head) = fmap f tail :< f head
+
 instance Applicative ReverseList where
+  pure x = REmpty :< x
+  liftA2 f (tailA :< headA) b = liftA2 f tailA b <> fmap (f headA) b
+  liftA2 f _ _ = REmpty
+
+
 instance Monad ReverseList where
+  (>>=) REmpty _ = REmpty
+  (>>=) (tail :< head) f = (>>=) tail f <> f head
